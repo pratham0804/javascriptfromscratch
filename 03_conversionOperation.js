fc02a792b10b5f9ff13cb2f752d5764df0892c3a
pratham0804 <149933759+pratham0804@users.noreply.github.com>
2024-01-22 20:08:36 +0000

    conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,45 @@
+" use strict"
+// let age = "33"
+// let age2 = "33abc"
+// let boolean = true
+// console.log(typeof age);
+// console.log(typeof age2);
+// console.log(typeof boolean);
+
+
+
+// let value =Number(age)
+// let value2= Number(age2);
+// let value3=Number(boolean);
+// console.log(typeof value);
+// console.log(typeof value2);
+// console.log(typeof value3);
+
+
+// console.log(value);
+// console.log(value2);
+// console.log(value3);
+
+
+// "33"  =>  number
+// "33abc" => NaN
+ // true =>1 ; false =>0;
+
+
+ // all the variables will be typecasted to number , but if we print then scenario is different 
+ let isloggedin= ""
+
+ let booleanIsloggedin = Boolean(isloggedin);
+ console.log(booleanIsloggedin);
+
+
+// 1 => true;
+// 0=> false;
+// "" => false ;
+// "Prathamesh" => true  
+
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
