21648b87f40e44feef0a064dc7e0d95276d68d76
pratham0804 <149933759+pratham0804@users.noreply.github.com>
2024-01-22 19:19:38 +0000

    datatypes covered

---
+++ 02_datatypes.js
@@ -0,0 +1,38 @@
+// code readability should be high ..
+"use strict"; // treat all JS code as newer version. 
+
+let name ="Prathamesh"
+
+let age = 19
+let islogin = true 
+
+// numbers  ki range fixed hai
+// bigint datatype use kr  skte 
+// string 
+// boolean
+// null => standalone value
+// undefined => value is not defined
+// symbol => fiding uniqueness between individual components
+
+
+
+// object =>
+
+// undefined vs null 
+// null is a representation of empty value ,
+// example :->we request current temperature to the server ,
+// seerver is not in a good state , and returns absurd value , so in that case
+// we prefer return of null 
+// which is nothing but empty 
+// undefined is different 
+// when variable is not defined , values is not given to the keys , we get undefined as result .
+
+
+console.log(typeof age);
+//  answer number print hokar aa raha hai , age ki float value dalne pr bhi number hi print ho raha hai 
+
+console.log(typeof null);
+// object print hokar aaega , null is a object..
+
+console.log(typeof undefined);
+//undefined print hokar aaega , kyoki vo ek type hai.
