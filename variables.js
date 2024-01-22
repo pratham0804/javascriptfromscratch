7bbd966e6b41a2b5cfdf2bd628e580e11d4c7bef
pratham0804 <149933759+pratham0804@users.noreply.github.com>
2024-01-22 19:19:13 +0000

    variables are discussed

---
+++ variables.js
@@ -0,0 +1,24 @@
+const Sachin_runs = 35000;
+let Virat_runs=29000;
+var Rohit_runs=18000;
+let Prathamesh ;  // sirf declare kiya hai ,    undefined print hokar aaega
+    // prefer the use of let and const , and avoid var , because it takes to the situation where you might face variable scope issue.
+
+ //Shubnam_runs=3500;
+
+
+
+// console.log(Sachin_runs);  It shows error as keys assigned under const keyword can't be changed.
+
+// use let or const.
+Rohit_runs=45345;
+Virat_runs=78978;
+
+
+console.table([Rohit_runs,Virat_runs,Sachin_runs,Prathamesh]);
+
+/* {
+this is scope '{}'....
+
+}
+*/
